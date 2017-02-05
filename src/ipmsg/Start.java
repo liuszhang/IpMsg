1a9908cced335e8327e243f1452102d731179f0e
盛章 刘 <liuszhang@qq.com>
2017-02-05 15:38:34 +0800

    change start.java

--- src/ipmsg/Start.java
+++ src/ipmsg/Start.java
@@ -31,7 +31,7 @@ import java.io.UnsupportedEncodingException;
 /**
  *
  * @author  Noeru
- * @checkby  liusz123
+ * @checkby  liusz123456
  */
 public class Start extends javax.swing.JFrame implements 
         InterfaceFreshUsers_Start{
